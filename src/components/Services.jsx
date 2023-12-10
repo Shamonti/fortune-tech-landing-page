2685a4aa50192521321aa47fc3428275710c0863
Shamonti <67649277+Shamonti@users.noreply.github.com>
2023-12-10 18:00:38 +0600

    Added services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,75 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import UI from '../assets/ui_ux.png';
+import Ecommerce from '../assets/ecommerce.png';
+import App from '../assets/mobile_apps.png';
+import Marketing from '../assets/digital_marketing.png';
+
 function Services() {
-  return <div>Services</div>;
+  return (
+    <section className='services-section section-padding'>
+      <Container>
+        <Row>
+          <h2 className='heading'>Quality Services</h2>
+          <p className='sub-heading'>Top notch services we offer</p>
+        </Row>
+        <Row className='pt-3'>
+          <Col md={3}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant='top' src={UI} />
+
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the cards content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={3}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant='top' src={Ecommerce} />
+
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the cards content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={3}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant='top' src={App} />
+
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the cards content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={3}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant='top' src={Marketing} />
+
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the cards content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
 
 export default Services;
