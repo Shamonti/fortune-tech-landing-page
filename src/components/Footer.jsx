1a7abf310bed8f9be27db7e9b0eccbd5fc84daf1
Shamonti <67649277+Shamonti@users.noreply.github.com>
2023-12-10 23:43:04 +0600

    Added the stats section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,41 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
-  )
+    <footer>
+      <Container>
+        <Row>
+          <Col md={4}>
+            <h5>Column 1</h5>
+            <ul className='list-unstyled'>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+              {/* Add more items as needed */}
+            </ul>
+          </Col>
+          <Col md={4}>
+            <h5>Column 2</h5>
+            <ul className='list-unstyled'>
+              <li>Item 4</li>
+              <li>Item 5</li>
+              <li>Item 6</li>
+              {/* Add more items as needed */}
+            </ul>
+          </Col>
+          <Col md={4}>
+            <h5>Column 3</h5>
+            <ul className='list-unstyled'>
+              <li>Item 7</li>
+              <li>Item 8</li>
+              <li>Item 9</li>
+              {/* Add more items as needed */}
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,85 @@
+import { Card, CardBody, Col, Container, Row } from 'react-bootstrap';
+import CountUp from 'react-countup';
+
 function Stats() {
-  return <div>Stats</div>;
+  return (
+    <section className='section-stats section-padding'>
+      <Container>
+        <Row>
+          <Col>
+            <h2 className='heading'>
+              We are in <span className='green-text'>numbers</span>
+            </h2>
+          </Col>
+        </Row>
+        <Row className='pt-5'>
+          <Col>
+            <Card>
+              <CardBody>
+                <div>
+                  <CountUp delay={3} end={7} />+
+                </div>
+                <p>Years of Experience</p>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardBody>
+                <div>
+                  <CountUp delay={3} end={7} />+
+                </div>
+                <p>Expert Engineers</p>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardBody>
+                <div>
+                  <CountUp delay={3} end={5} />+
+                </div>
+                <p>LTUs Are Using</p>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row className='pt-5'>
+          <Col>
+            <Card>
+              <CardBody>
+                <div>
+                  <CountUp delay={3} end={250} />+
+                </div>
+                <p>Happy Companies</p>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardBody>
+                <div>
+                  <CountUp delay={3} end={7} />+
+                </div>
+                <p>Project Completed</p>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardBody>
+                <div>
+                  <CountUp delay={3} end={250} />
+                  K+
+                </div>
+                <p>People Are Using</p>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
 
 export default Stats;
